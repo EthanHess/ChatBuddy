2ead05030b8d4f6bdc60039421ca456b6aadfa5b
Ethan Hess <ech1988@gmail.com>
2026-05-26 13:10:45 -0700

    UI updates for message bubbles

--- ChatBuddy/ChatBuddy/Extensions.swift
+++ ChatBuddy/ChatBuddy/Extensions.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @available(iOS 15.0, *)
 extension View {
+    //MARK: Backgrounds / layers
     var blueNeon: some View {
         self.background(
             RoundedRectangle(cornerRadius: 10)
@@ -25,4 +26,58 @@ extension View {
                 .stroke(Color.cyan.opacity(0.25), lineWidth: 1)
         )
     }
+    
+    var orangeNeon: some View {
+        self.background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(
+                    LinearGradient(
+                        colors: [Color.orange.opacity(0.2), .black],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                )
+        )
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color(uiColor: .lightOrange.withAlphaComponent(0.25)), lineWidth: 1)
+        )
+    }
+    
+    var greenNeon: some View {
+        self.background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(
+                    LinearGradient(
+                        colors: [Color.green.opacity(0.2), .black],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                )
+        )
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color(uiColor: .lightGreen.withAlphaComponent(0.25)), lineWidth: 1)
+        )
+    }
+
+    func neon(_ style: NeonStyle) -> some View {
+        switch style {
+        case .orange:
+            AnyView(self.orangeNeon)
+        case .green:
+            AnyView(self.greenNeon)
+        }
+    }
+}
+
+enum NeonStyle {
+    case orange
+    case green
+}
+
+
+extension UIColor {
+    static let lightOrange = UIColor(red: 255/255, green: 221/255, blue: 86/255, alpha: 1.0)
+    static let lightGreen = UIColor(red: 102/255, green: 229/255, blue: 73/255, alpha: 1.0)
 }
--- ChatBuddy/ChatBuddy/MessageBubble.swift
+++ ChatBuddy/ChatBuddy/MessageBubble.swift
@@ -15,19 +15,18 @@ struct MessageBubble: View {
         HStack {
             if authorUID == 0 { Spacer() }
             HStack(spacing: 10) {
-                Image(systemName: "globe.americas.fill")
+                Image(systemName: "person.circle.fill")
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 50)
+                    .frame(width: 40)
                 Text(messageBody)
                     .lineLimit(nil)
             }
             .padding(12)
-            .background(authorUID == 0 ? Color.cyan : Color.green)
             .cornerRadius(10)
             .frame(maxWidth: 250, alignment: authorUID == 0 ? .trailing : .leading)
             if authorUID == 1 { Spacer() }
-        }
+        }.neon(authorUID == 0 ? .green : .orange)
         .padding(.horizontal)
     }
 }
